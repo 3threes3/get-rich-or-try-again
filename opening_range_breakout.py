01ac4593478f0b13aa14ad716898844e02326c27
3threes3 <43138471+3threes3@users.noreply.github.com>
2021-05-27 20:51:10 +0200

    Added control on opening range breakout strategy for missing prices

--- opening_range_breakout.py
+++ opening_range_breakout.py
@@ -1,4 +1,6 @@
 import sqlite3
+
+from numpy import empty
 import config
 import alpaca_trade_api as tradeapi
 import smtplib, ssl
@@ -47,44 +49,48 @@ for symbol in symbols:
     opening_range_mask = (minute_bars.index >= start_minute_bar) & (minute_bars.index < end_minute_bar)
     opening_range_bars = minute_bars.loc[opening_range_mask]
 
-    opening_range_low = opening_range_bars['low'].min()
-    opening_range_high = opening_range_bars['high'].max()
-    opening_range = opening_range_high - opening_range_low
+    if not opening_range_bars.empty:
 
-    after_opening_range_mask = minute_bars.index >= end_minute_bar
-    after_opening_range_bars = minute_bars.loc[after_opening_range_mask]
-    print(f'############## {symbol} #############')
-    print(after_opening_range_bars)
+        opening_range_low = opening_range_bars['low'].min()
+        opening_range_high = opening_range_bars['high'].max()
+        opening_range = opening_range_high - opening_range_low
 
-    after_opening_range_breakout = after_opening_range_bars[after_opening_range_bars['close'] > opening_range_high]
-    
-    messages.append(f"no\n whatever bro \n like I said, whatever")
+        after_opening_range_mask = minute_bars.index >= end_minute_bar
+        after_opening_range_bars = minute_bars.loc[after_opening_range_mask]
+        print(f'############## {symbol} #############')
+        print(after_opening_range_bars)
 
-    if not after_opening_range_breakout.empty:
-        if symbol not in existing_order_symbols:
-            limit_price = after_opening_range_breakout.iloc[0]['close']
+        after_opening_range_breakout = after_opening_range_bars[after_opening_range_bars['close'] > opening_range_high]
 
-            messages.append(f"Placing order for {symbol} at {limit_price}, closed above {opening_range_high}\n\n{after_opening_range_breakout.iloc[0]['close']}\n\n")
-            
-            print(f"Placing order for {symbol} at {limit_price}, closed above {opening_range_high} at {after_opening_range_breakout.iloc[0]}")
+        if not after_opening_range_breakout.empty:
+            if symbol not in existing_order_symbols:
+                limit_price = after_opening_range_breakout.iloc[0]['close']
 
-            api.submit_order(
-                symbol=symbol,
-                side='buy',
-                type='limit', 
-                qty='100',
-                time_in_force='day',
-                order_class='bracket',
-                limit_price = limit_price,
-                take_profit=dict(
-                    limit_price=limit_price + opening_range + 0.01,
-                ),
-                stop_loss=dict(
-                    stop_price=limit_price - opening_range - 0.01,
+                messages.append(f"Placing order for {symbol} at {limit_price}, closed above {opening_range_high}\n\n{after_opening_range_breakout.iloc[0]['close']}\n\n")
+                
+                print(f"Placing order for {symbol} at {limit_price}, closed above {opening_range_high} at {after_opening_range_breakout.iloc[0]}")
+
+                api.submit_order(
+                    symbol=symbol,
+                    side='buy',
+                    type='limit', 
+                    qty='100',
+                    time_in_force='day',
+                    order_class='bracket',
+                    limit_price = limit_price,
+                    take_profit=dict(
+                        limit_price=limit_price + opening_range + 0.02,
+                    ),
+                    stop_loss=dict(
+                        stop_price=limit_price - opening_range - 0.2,
+                    )
                 )
-            )
-        else:
-            print(f'Conditions met, but order for {symbol} already existing. Skipping...')
+            else:
+                print(f'Conditions met, but order for {symbol} already existing. Skipping...')
+                messages.append(f'Conditions met, but order for {symbol} already existing.')
+            
+    else:
+        messages.append(f'Unable to retrieve sufficient data to place order for {symbol}')
 
 print(messages)
 
